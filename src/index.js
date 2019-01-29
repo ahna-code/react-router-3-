4b7f1a6c45eed6614a0404b7b3632b2da8c64eb0
Agnieszka Korczak <korczak.a.m@gmail.com>
2019-01-29 20:19:39 +0100

    Add project content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router'
+import { Provider } from 'react-redux';
+import store from './store';
+import { getCountries } from '../src/actions/actions-countries';
+import routes from './routes';
+import './country.css'
+import DevTools from './DevTools';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+    <Provider store={store}>
+        <div>
+            <Router history={hashHistory} routes={routes}/>
+            <DevTools />
+        </div>
+    </Provider>,
+    document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+store.dispatch(getCountries());
