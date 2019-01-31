e4a84ed8cf16c991906efaee50ab5999962f4967
Agnieszka Korczak <korczak.a.m@gmail.com>
2019-01-31 15:10:08 +0100

    Update files

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import { render } from 'react-dom';
 import { Router, hashHistory } from 'react-router'
 import { Provider } from 'react-redux';
 import store from './store';
-import { getCountries } from '../src/actions/actions-countries';
 import routes from './routes';
-import './country.css'
-import DevTools from './DevTools';
 
 render(
     <Provider store={store}>
-        <div>
-            <Router history={hashHistory} routes={routes}/>
-            <DevTools />
-        </div>
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
 
-
-store.dispatch(getCountries());
